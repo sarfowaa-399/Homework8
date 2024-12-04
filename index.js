323a0a0a74f4edba0db992fb4ca8db7875971274
Your Name <amasarfowaap@gmail.com>
2024-12-04 09:07:35 +0000

    added files in my index.html and index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function checkGender(){
+}let gender = prompt ("Please enter your gender  (Male or Female):");
+if (gender === "Female"){
+    alert("You are a girl ! 👧");
+}else if(gender === "Male"){
+    alert("You are a Boy ! 👦");
+}else{
+    alert("invalid input. please enter a valid gender 'Male' or 'Female' ");
+}
+checkGender();
